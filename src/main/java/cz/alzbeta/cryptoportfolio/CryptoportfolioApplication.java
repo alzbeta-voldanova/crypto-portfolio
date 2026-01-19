09c4883c3d6f9cd4d4ecc561c110f9442092388a
Alzbeta V. <alzbeta.voldanova@gmail.com>
2026-01-19 18:20:52 +0100

    init spring boot project

---
+++ src/main/java/cz/alzbeta/cryptoportfolio/CryptoportfolioApplication.java
@@ -0,0 +1,13 @@
+package cz.alzbeta.cryptoportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoportfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoportfolioApplication.class, args);
+	}
+
+}
